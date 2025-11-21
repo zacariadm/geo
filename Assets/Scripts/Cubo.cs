bdc19aa86c335ce3713faa64790c5b959f79dbc8
zacaria_samuel_rodri <zacariadm@gmail.com>
2025-11-21 12:31:29 +0100

    v2

--- Assets/Scripts/Cubo.cs
+++ Assets/Scripts/Cubo.cs
@@ -1,9 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+public enum Speeds { Lento = 0, Normal = 1, Rapido = 2, MuyRapido = 3, Rapidisimo = 4}; // enum para darle determinadas velocidades
 public class PlayerMovement : MonoBehaviour
 {
-   [SerializeField] public float moveSpeed = 7f;      // Velocidad hacia adelante
+    [SerializeField] public Speeds velocidad; // declaramos índice de velocidades
+
+    //                                             0       1       2      3      4
+    [SerializeField] float [] valoresVelocidad = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f};
+
+    public Transform tocaSuelo;
+    public float radioTocaSuelo;
+    public LayerMask capaSuelo;
+
+    Rigidbody2D rd;
+
+    void Start()
+    {
+        //https://youtu.be/h0qwFV6cpOs?list=PLCmEmqCMwg-U77rz6O3ND-WQ1395f4NW1
+        rd.linearVelocity = Vector2.zero;
+        rd = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        transform.position += Vector3.right * valoresVelocidad[(int)velocidad] * Time.deltaTime;
+
+        if(Mouse.current.leftButton.isPressed)
+        {
+            if (OnGround())
+            {
+                rd.AddForce(Vector2.up * 26.6581f, ForceMode2D.Impulse);
+            }
+        }
+
+        bool OnGround()
+        {
+            return Physics2D.OverlapCircle(tocaSuelo.position, radioTocaSuelo, capaSuelo );
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+  /* [SerializeField] public float moveSpeed = 7f;      // Velocidad hacia adelante
     [SerializeField] public float jumpHeight = 2f;     // Altura del salto sin físicas
     [SerializeField] public InputActionReference saltar;  // Acción de salto del Input System
 
@@ -52,4 +98,6 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = false;
         }
     }
+  */
 }
+
